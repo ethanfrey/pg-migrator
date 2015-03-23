32899a619cdf215ff83cd066caacf2c19bb84da4
Ethan <ethansf@github.com>
2015-03-23 16:28:29 +0000

    Use psycopg2 string formatting - mogrify

--- transform.py
+++ transform.py
@@ -9,45 +9,51 @@ except ImportError:
     import ijson
 
 
-def json_to_sql(item):
+def json_to_sql(item, cur):
     if item['kind'] == 'insert':
-        return insert_to_sql(item)
+        return insert_to_sql(item, cur)
     elif item['kind'] == 'update':
-        return update_to_sql(item)
+        return update_to_sql(item, cur)
     elif item['kind'] == 'delete':
-        return delete_to_sql(item)
+        return delete_to_sql(item, cur)
     else:
         # TODO: error handling
         print "*** ERROR ***"
         return ''
 
 
-def update_to_sql(item):
-    pairs = zip(item['oldkeys']['keynames'], item['oldkeys']['keyvalues'])
-    # TODO: only works well with integers, not with strings as primary values
-    query = ', '.join('{}={}'.format(name, value) for name, value in pairs)
-
-    val_pairs = zip(item['columnnames'], item['columnvalues'])
-    values = ', '.join('{}={}'.format(name, value) for name, value in val_pairs)
-
-    sql = "UPDATE {} SET {} WHERE {}".format(item['table'], values, query)
-    return sql
+def _make_pair_placeholder(names):
+    return ', '.join('{}=%s'.format(x) for x in names)
 
 
-def insert_to_sql(item):
-    sql = "INSERT INTO {} ({}) VALUES ({})".format(item['table'], item['columnnames'], item['columnvalues'])
-    return sql
+def update_to_sql(item, cur):
+    query_placeholder = _make_pair_placeholder(item['oldkeys']['keynames'])
+    set_placeholder = _make_pair_placeholder(item['columnnames'])
+    sql = "UPDATE {} SET {} WHERE {};".format(item['table'], set_placeholder, query_placeholder)
+    sql_values = item['columnvalues'] + item['oldkeys']['keyvalues']
+    return cur.mogrify(sql, sql_values)
 
 
-def delete_to_sql(item):
-    pairs = zip(item['oldkeys']['keynames'], item['oldkeys']['keyvalues'])
-    # TODO: only works well with integers, not with strings as primary values
-    query = ', '.join('{}={}'.format(name, value) for name, value in pairs)
-    sql = "DELETE FROM {} WHERE {}".format(item['table'], query)    
-    return sql
+def insert_to_sql(item, cur):
+    columns = ', '.join(item['columnnames'])
+    value_placeholder = ', '.join(['%s'] * len(item['columnvalues']))
+    sql = "INSERT INTO {} ({}) VALUES ({});".format(item['table'], columns, value_placeholder)
+    sql_values = item['columnvalues']
+    return cur.mogrify(sql, sql_values)
+
+
+def delete_to_sql(item, cur):
+    query_placeholder = _make_pair_placeholder(item['oldkeys']['keynames'])
+    sql = "DELETE FROM {} WHERE {};".format(item['table'], query_placeholder)  
+    sql_values = item['oldkeys']['keyvalues']
+    return cur.mogrify(sql, sql_values)
 
 
 def transform(item):
+    # only transform these calls
+    if item['kind'] not in set(['update', 'insert']):
+        return item
+    # and only on the appropriate table
     # TODO: callback to register transforms
     if item['table'] == 'data':
         item['columnnames'].append('new_data')
@@ -57,13 +63,12 @@ def transform(item):
 
 def main(connect_params):
     conn = psycopg2.connect(connect_params)
-    cur = conn.cursor()
     parsed = ijson.parse(sys.stdin, multiple_values=True, buf_size=256)
-    for item in ijson.common.items(parsed, 'change.item'):
-        # print "GOT ITEM: {}".format(item)
-        if item['kind'] in set(['update', 'insert']):
-            item = transform(item)
-        print json_to_sql(item)
+    for change_list in ijson.common.items(parsed, 'change'):
+        with conn.cursor() as cur:
+            for item in change_list:
+                transformed = transform(item)
+                print json_to_sql(transformed, cur)
 
 
 if __name__ == '__main__':
